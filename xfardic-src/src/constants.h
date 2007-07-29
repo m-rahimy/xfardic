34b9c946c8949d3217c8a1c66978f4a8007d4f12
alanbach <alanbach@017a01de-341e-0410-93c4-b9013b265235>
2007-07-29 12:46:06 +0000

    Bump version to 0.9.99.2

--- xfardic-src/src/constants.h
+++ xfardic-src/src/constants.h
@@ -31,7 +31,7 @@
 // constants
 // ----------------------------------------------------------------------------
 
-#define XVERSION  _T("0.9.99.1")
+#define XVERSION  _T("0.9.99.2")
 
 // IDs for the controls and the menu commands
 enum
