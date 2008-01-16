6aab24524674ac1a6affc091f98a587d1947f4ff
alanbach <alanbach@017a01de-341e-0410-93c4-b9013b265235>
2008-01-16 09:46:24 +0000

    * Updated Makefiles, manpage and version for 0.10.3

--- xfardic-src/src/constants.h
+++ xfardic-src/src/constants.h
@@ -31,7 +31,7 @@
 // constants
 // ----------------------------------------------------------------------------
 
-#define XVERSION  _T("0.10.2")
+#define XVERSION  _T("0.10.3")
 
 // IDs for the controls and the menu commands
 enum
