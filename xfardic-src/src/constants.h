a38d94974054ed28beb9649fed7807c1c5f6b583
alanbach <alanbach@017a01de-341e-0410-93c4-b9013b265235>
2008-01-23 06:17:36 +0000

    * Added 0.10.4 regular updates

--- xfardic-src/src/constants.h
+++ xfardic-src/src/constants.h
@@ -31,7 +31,7 @@
 // constants
 // ----------------------------------------------------------------------------
 
-#define XVERSION  _T("0.10.3")
+#define XVERSION  _T("0.10.4")
 
 // IDs for the controls and the menu commands
 enum
